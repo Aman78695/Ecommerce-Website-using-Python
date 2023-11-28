53a24deafe62eca02531364e057c767089c52f5c
amanatulla1606@gmail.com <amanatulla1606@gmail.com>
2023-11-28 16:43:20 +0000

    Created MyCourses; Edited settings, Base, Base.Home and Courses

--- client_code/Base/__init__.py
+++ client_code/Base/__init__.py
@@ -1,6 +1,13 @@
 from ._anvil_designer import BaseTemplate
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 from .Home import Home
+from ..MyCourses import MyCourses
 
 class Base(BaseTemplate):
   def __init__(self, **properties):
@@ -13,5 +20,14 @@ class Base(BaseTemplate):
   def link_1_click(self, **event_args):
     self.content_panel.clear()
     self.content_panel.add_component(Home())
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(MyCourses())
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    anvil.users.login_with_form()
     
   
--- client_code/MyCourses/__init__.py
+++ client_code/MyCourses/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import HomeTemplate
+from ._anvil_designer import MyCoursesTemplate
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
@@ -6,16 +6,10 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
-from ...Courses import Courses
 
-class Home(HomeTemplate):
+class MyCourses(MyCoursesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.column_panel_1.clear()
-    self.column_panel_1.add_component(Courses())
